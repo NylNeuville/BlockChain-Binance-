22078c6a04807d07254cb4f47dbf9fd012d2211a
Nyl <95429032+NylNeuville@users.noreply.github.com>
2022-07-14 14:25:08 +0200

    Initial Commit

---
+++ TestAPIBinance/TestAPIBinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPIBinanceApp.swift
+//  TestAPIBinance
+//
+//  Created by Nyl Neuville on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAPIBinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
